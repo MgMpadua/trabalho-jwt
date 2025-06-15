b9b550275c0faf7e0d6c9e015f039482070aea95
MatheusPadua <166460039+MgMpadua@users.noreply.github.com>
2025-06-15 20:46:28 -0300

    Add files via upload

---
+++ trabalho/src/main/java/com/example/trabalho/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.trabalho.security;
+
+import com.example.trabalho.Model.UserModel;
+import com.example.trabalho.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("email de usuario não encontrado" + email));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getSenha(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole())));
+    }
+}
